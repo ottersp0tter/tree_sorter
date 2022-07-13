be79b722c0516e5a0c914190773aa9eddf5fa1c0
otterSp0tter <hep.crossing@gmail.com>
2022-07-13 22:13:09 +1000

    added number dict

--- tree_sorter_code.py
+++ tree_sorter_code.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 import pandas as pd
+from collections import namedtuple
  
 file = "tree_sorter.xlsx"
 data = pd.read_excel(file) #reading file
@@ -19,41 +20,71 @@ def is_tree(x):
             return True
     return False
 
+#making a word number dictionary, eg 25 is Twenty-five
+number_words_dict = {}
+with open("numbers.csv", "r") as f:
+    lines = f.readlines()
 
 
-four_D = []
-four_C = []
-four_B = []
-four_A = []
+for line in lines:
+    split_line = line.split(",")
+    number = int(split_line[0])
+    word = split_line[1]
+    if  "\n" in word:
+        size = len(word)
+        word = word[:size - 1]
+    number_words_dict[number] = word
 
+#_________________________________________________________#
+
+
+
+# sentence making function
+probability = {'a':'very likely', 'b':'likely', 'c':'somewhat likely', 'd':'unlikely','e':'highly unlikely'}
+
+occupation = {'1':'occasional', '2':'intermittent', '3':'frequent', '4':'constant','5':'continuous'}
+
+risk_level = {
+    '5a':'extreme',
+    '5b':'extreme', 
+    '5c':'very high',
+    '5d':'medium',
+    '5e':'ALARP',
+    '4a':'very high',
+    '4b':'very high',
+    '4c':'high',
+    '4d':'medium',
+    '4e':'ALARP',
+    '3a':'high',
+    '3b':'high',
+    '3c':'high',
+    '3d':'medium',
+    '2a':'high',
+    '2b':'medium',
+    '2c':'medium',
+    '2d':'ALARP',
+    '2e':'ALARP',
+    '1a':'medium',
+    '1b':'medium',
+    '1c':'ALARP',
+    '1d':'ALARP',
+    '1e':'ALARP'
+    }
+
+def sentence_maker(my_list, risk):
+    my_string = ', '.join(str(x) for x in my_list)
+    my_count = len(my_list)
+    word_count = number_words_dict[my_count]
+    print(word_count + " (" + str(my_count) + ")" + ' trees numbered ' + my_string + ' have a ' + risk_level[risk] + ' risk rating (' + risk + ') as they are ' + probability[risk[1]] + ' to fail in a ' + occupation[risk[0]] + ' use area.')
+
+
+tree_data = 
 for i in range(num_rows):
     tree_name = data_dict['Tree'][i]
     tree_risk = data_dict['Tree Risk Rating'][i]
-    if is_tree(tree_name):
-        if is_tree(tree_risk):
-            if '4a' in tree_risk:
-                four_A.append(tree_name)
-            if '4b' in tree_risk:
-                four_B.append(tree_name)
-            if '4c' in tree_risk:
-                four_C.append(tree_name)
-            if '4d' in tree_risk:
-                four_D.append(tree_name)
 
-#4d
-four_D_str = ', '.join(str(x) for x in four_D)
-four_D_sentence = str(len(four_D)) + ' trees numbered ' + four_D_str + ' have a medium risk rating (4D) as they are unlikely to fail in a constant use area.'
+    # if is_tree(tree_name):
+    #     if is_tree(tree_risk):
 
-#4c
-four_C_str = ', '.join(str(x) for x in four_C)
-four_C_sentence = str(len(four_C)) + ' trees numbered ' + four_C_str + ' have a high risk rating (4C) as they are somewhat likely to fail in a constant use area.'
-
-#4b
-four_B_str = ', '.join(str(x) for x in four_B)
-four_B_sentence = str(len(four_B)) + ' trees numbered ' + four_B_str + ' have a very high risk rating (4B) as they are likely to fail in a constant use area.'
-
-#4a
-four_A_str = ', '.join(str(x) for x in four_A)
-four_A_sentence = str(len(four_A)) + ' trees numbered ' + four_A_str + ' have a very high risk rating (4A) as they are highly likely to fail in a constant use area.'
-
-print(four_A_sentence + "\n" + four_B_sentence + "\n" + four_C_sentence + "\n" + four_D_sentence)
+            # if '4d' in tree_risk:
+            #     four_D.append(tree_name)
